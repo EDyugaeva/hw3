251a84542f92ba1612933253470cfa4929ffce55
Ekaterina <ed2408@yandex.ru>
2021-11-23 18:57:59 +0300

    First commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -17,7 +17,9 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке ");
         } else if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else System.out.println("Ошибка");
+        } else {
+            System.out.println("Ошибка");
+        }
     }
 
     public static void task2() {
@@ -32,19 +34,21 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (apple && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
     }
 
+
     public static void task3() {
-        short year = 2024;
-        byte temp1 = (byte) (year % 4);
-        byte temp2 = (byte) (year % 100);
-        byte temp3 = (byte) (year % 400);
-        if (temp3 == 0) {
+        float year = 2024f;
+        if (year % 4 == 0) {
             System.out.println("Год  является високосным");
-        } else if (temp1 == 0 && temp2 != 0) {
+        } else if (year % 4 == 0 && year % 100 == 0) {
             System.out.println("Год  является високосным");
-        } else System.out.println("Год не является високосным");
+        } else {
+            System.out.println("Год не является високосным");
+        }
     }
 
     public static void task4() {
@@ -52,10 +56,12 @@ public class Main {
         short deliveryDay;
         if (deliveryDistance <= 20) {
             deliveryDay = 1;
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+        } else if (deliveryDistance <= 60) {
             deliveryDay = 2;
-        } else deliveryDay = 3;
-        System.out.println("Потребуется дней: " + deliveryDay);
+        } else {
+            deliveryDay = 3;
+            System.out.println("Потребуется дней: " + deliveryDay);
+        }
     }
 
     public static void task5() {
@@ -81,7 +87,7 @@ public class Main {
             case 11:
                 System.out.println("Осень");
                 break;
-                       default:
+            default:
                 System.out.println("Ошибка! Нет такого месяца");
         }
     }
